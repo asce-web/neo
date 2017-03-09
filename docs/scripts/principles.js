da5b686fda4386424db0142ca5b0cfda646beca1
Chris Harvey (work) <charvey@asce.org>
2017-03-09 11:13:43 -0500

    add assets

--- docs/scripts/principles.js
+++ docs/scripts/principles.js
@@ -1,10 +1,9 @@
 var Color = require('csscolor').Color
-var ConfSite = require('../../_models/ConfSite.class.js')
 
 $('input.js-picker').change(function () {
   var primary = Color.fromString($('input.js-picker-primary').val())
   var secondary = Color.fromString($('input.js-picker-secondary').val())
-  var styleobj = ConfSite.colorStyles(primary, secondary)
+  var styleobj = require('../../_models/ConfSite.class.js').colorStyles(primary, secondary)
   for (var prop in styleobj) {
     document.querySelector('.docs-o-ColorList#docs-colors').style.setProperty(prop, styleobj[prop])
   }
