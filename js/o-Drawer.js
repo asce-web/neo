38b6a12d20372ca20f1df1de893d88e42aee56ee
Chris Harvey (work) <charvey@asce.org>
2017-10-27 16:24:29 -0400

    fix drawer tabindices

--- js/o-Drawer.js
+++ js/o-Drawer.js
@@ -1,10 +1,12 @@
+$('.o-Drawer__Bar:not(.o-Drawer__Bar--js-open)').find('a').attr('tabindex', -1);
 $('.o-Drawer__Button').click(function () {
   var drawer = $(this).siblings('.o-Drawer__Bar');
   var is_open = drawer.attr('aria-expanded') === 'true';
   drawer
     .toggleClass('o-Drawer__Bar--js-open')
     .attr('aria-expanded', !is_open)
-    .attr('aria-hidden'  ,  is_open);
+    .attr('aria-hidden'  ,  is_open)
+    .find('a').attr('tabindex', (is_open) ? -1 : 0);
   $(this).toggleClass('-ps-f')
     .find('.c-MobileMenuToggle').toggleClass('c-MobileMenuToggle--js-close')
 });
