ba89e91fcb27533bc7a7f63f27f6c6263711c59c
Chris Harvey <chrisharvey2pi@gmail.com>
2018-03-22 19:53:51 -0400

    deleted:    class/Pass.class.js

--- class/Conference.class.js
+++ class/Conference.class.js
@@ -4,7 +4,6 @@ const xjs = {
 }
 const View = require('extrajs-view')
 
-const Pass               = require('./Pass.class.js')
 const PostalAddress      = require('./PostalAddress.class.js')
 const Venue              = require('./Venue.class.js')
 const Person             = require('./Person.class.js')
