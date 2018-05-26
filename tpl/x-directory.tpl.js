b04e94df6a9bb963fc43ec0bf84912f1144e8f41
Chris Harvey <chrisharvey2pi@gmail.com>
2018-05-25 21:46:00 -0400

    simplify xDirectory_renderer opts params

--- tpl/x-directory.tpl.js
+++ tpl/x-directory.tpl.js
@@ -15,39 +15,37 @@ const xjs = {
  * @param   {integer=} options.start which subpage to start at
  * @param   {integer=} options.end which subpage to end at
  * @param   {?Object<string>=} opts.classes group set of css class configurations
- * @param   {string=} opts.classes.list list classes (`<ol>`)
- * @param   {string=} opts.classes.item item classes (`<li>`)
- * @param   {!Object=} opts.links configuration param to send into {@link Util.VIEW.pageLink|Util#view.pageLink()}
+ * @param   {string=}          opts.classes.list list classes (`<ol>`)
+ * @param   {string=}          opts.classes.item item classes (`<li>`)
+ * @param   {string=}          opts.classes.link link classes (`<a>`)
+ * @param   {string=}          opts.classes.icon classes for page icon
+ * @param   {string=}          opts.classes.expand classes for `expand_more` icon
  * @param   {!Object=} opts.options configurations for nested outlines; specs identical to `opts`
 */
 function xDirectory_renderer(frag, data, opts = {}) {
   const Util = require('../class/Util.class.js')
   let subpages = (xjs.Object.typeOf(data.hasPart) === 'array' ) ? data.hasPart : [data.hasPart]
   let depth    = (xjs.Object.typeOf(opts.depth)   === 'number') ? opts.depth   : Infinity
-  let classes  = opts.classes || {}
-  let links    = opts.links || {}
-  let suboptions = opts.options || {}
   new xjs.HTMLOListElement(frag.querySelector('ol'))
-    .replaceClassString('{{ classes.list }}', classes.list || '')
+    .replaceClassString('{{ classes.list }}', opts.classes && opts.classes.list || '')
     .populate(subpages, function (f, d, o) {
-      let linkclasses = links.classes || {}
-      new xjs.HTMLLIElement(f.querySelector('[itemprop="hasPart"]')).replaceClassString('{{ classes.item }}', classes.item || '')
+      new xjs.HTMLLIElement(f.querySelector('[itemprop="hasPart"]')).replaceClassString('{{ classes.item }}', opts.classes && opts.classes.item || '')
       new xjs.HTMLAnchorElement(f.querySelector('[itemprop="url"]'))
-        .replaceClassString('{{ classes.link }}', linkclasses.link || '')
+        .replaceClassString('{{ classes.link }}', opts.classes && opts.classes.link || '')
         .href(d.url()) // TODO don’t use Page#url()
       f.querySelector('slot[itemprop="name"]').textContent = d.name() // TODO don’t use Page#name()
 
       let icons = [...f.querySelectorAll('i.material-icons')]
-      if (xjs.Object.typeOf(linkclasses.icon) === 'string') {
+      if (xjs.Object.typeOf(opts.classes && opts.classes.icon) === 'string') {
         new xjs.HTMLElement(icons[0])
-          .replaceClassString('{{ classes.icon }}', linkclasses.icon || '')
+          .replaceClassString('{{ classes.icon }}', opts.classes && opts.classes.icon || '')
           .textContent(d.getIcon()) // TODO don’t use ConfPage#getIcon()
       } else {
         icons[0].remove()
       }
-      if (xjs.Object.typeOf(linkclasses.expand) === 'string' && d.findAll().length) { // TODO don’t use Page#findAll
+      if (xjs.Object.typeOf(opts.classes && opts.classes.expand) === 'string' && d.findAll().length) { // TODO don’t use Page#findAll
         new xjs.HTMLElement(icons[1])
-          .replaceClassString('{{ classes.expand }}', linkclasses.expand || '')
+          .replaceClassString('{{ classes.expand }}', opts.classes && opts.classes.expand || '')
       } else {
         icons[1].remove()
       }
@@ -58,7 +56,7 @@ function xDirectory_renderer(frag, data, opts = {}) {
             ...d,
             hasPart: d.findAll().filter((p) => !p.isHidden()),
           }, null, {
-            ...suboptions,
+            ...(opts.options || {}),
             depth: depth - 1,
           })
         )
