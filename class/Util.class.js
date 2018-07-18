f966658650d58ccf9fa7a2e6ff1e5c1d99d0b757
Chris Harvey <chrisharvey2pi@gmail.com>
2018-07-18 15:20:44 -0400

    deleted:    tpl/x-list-pass.tpl.{html,js}

--- class/Util.class.js
+++ class/Util.class.js
@@ -8,6 +8,7 @@ const xjs = {
 const View    = require('extrajs-view')
 
 const xDirectory          = require('../tpl/x-directory.tpl.js')
+const xPass = require('../tpl/x-pass.tpl.js')
 
 
 /**
@@ -199,7 +200,21 @@ class Util {
        * @returns {string} HTML output
        */
       .addDisplay(function pass($conference, queue = null) {
-        const xListPass = require('../tpl/x-list-pass.tpl.js')
+        const xListPass = xjs.HTMLUListElement.templateSync()
+          .exe(function () {
+            new xjs.HTMLUListElement(this.content().querySelector('ul')).addClass('o-List o-Flex o-ListStacked')
+            new xjs.HTMLLIElement(this.content().querySelector('template').content.querySelector('li'))
+              .addClass('o-List__Item o-Flex__Item o-ListStacked__Item')
+              .innerHTML(`<link rel="import" data-import="template" href="../tpl/x-pass.tpl.html"/>`)
+            new xjs.DocumentFragment(this.content().querySelector('template').content).importLinks(__dirname)
+          })
+          .setRenderer(function (frag, data, opts) {
+            new xjs.HTMLUListElement(frag.querySelector('ul')).populate(data, function (f, d, o) {
+              new xjs.HTMLLIElement(f.querySelector('li')).empty().append(
+                xPass.render(d, null, { conference: o.conference })
+              )
+            }, null, { conference: opts.conference })
+          })
         let item_keys = (xjs.Object.typeOf(queue) === 'object') ? queue.itemListElement || [] : queue
         let items = this.filter((item) => (queue) ? item_keys.includes(item.name) : true)
         return new xjs.DocumentFragment(xListPass.render(items, null, { conference: $conference })).innerHTML()
